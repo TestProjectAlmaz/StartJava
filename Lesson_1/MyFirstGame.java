d9439fe7956f7c6c55d7f80c032a6c65a7dcab01
AlmazTesla <almaz-tilla@yandex.ru>
2023-02-25 09:32:10 +0300

    first commit

---
+++ Lesson_1/MyFirstGame.java
@@ -0,0 +1,16 @@
+public class MyFirstGame {
+    public static void main (String[] args) {
+        int randomNumber = (int) (Math.random() * 100 + 1);
+        int playerNumber = 0;
+        System.out.println("Угадай число в промежутке от 0 до 100");
+        while (randomNumber != playerNumber) {
+            playerNumber = Integer.parseInt(System.console().readLine());
+            if (randomNumber < playerNumber) {
+                System.out.println("Это число больше того что загадал компьютер.");
+        } else {
+            System.out.println("Это число меньше того что загадал компьютер.");
+          }
+        }
+        System.out.println("Вы победили!");
+      }
+    }
