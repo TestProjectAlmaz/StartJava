09eb8701048b5937889ccae8b1632df8e82b301c
AlmazTesla <almaz-tilla@yandex.ru>
2023-03-19 10:26:29 +0300

    changes 19.03

--- Lesson_1/CyclesTheme.java
+++ Lesson_1/CyclesTheme.java
@@ -56,7 +56,7 @@ public class CyclesTheme {
 
         System.out.println("4. Вывод чисел на консоль в несколько строк:\n ");
         int start = 1;
-        int end = 24;
+        int end = 30;
         int step = 2;
         int numbersPerLine = 5;        
         int count2 = 0;
@@ -74,7 +74,8 @@ public class CyclesTheme {
                 count2 = 0;
             }
         }
-        int zerosNeeded = numbersPerLine - count2;       
+        //int zerosNeeded = numbersPerLine - count2; 
+         int zerosNeeded = 3;    
         for (int i = 0; i < zerosNeeded; i++) {
             System.out.printf("%3d ", i);
         }    
